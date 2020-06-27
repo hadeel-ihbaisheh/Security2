dcb491d04ec20d68057fc92fb0fe8d99e1317793
unknown <hbisheh.hadeel@gmail.com>
2020-06-27 12:02:59 +0300

    first commit

---
+++ Security1-master/src/securityhw1/SecurityHW1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package securityhw1;
+
+/**
+ *
+ * @author hp
+ */
+public class SecurityHW1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         NewJFrame f= new  NewJFrame();
+      f.setVisible(true);// TODO code application logic here // TODO code application logic here
+    }
+    
+}
